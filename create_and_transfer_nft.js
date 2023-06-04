592e389b487939b7cea1438d57b816d73c32408e
sabya <sabya7011@gmail.com>
2023-06-04 12:07:05 +0530

    mint a new set of nft

--- create_and_transfer_nft.js
+++ create_and_transfer_nft.js
@@ -55,6 +55,54 @@ async function createFirstNft() {
     //Log the token ID
     console.log(`- Created NFT with Token ID: ${tokenId} \n`);
   
+    // Max transaction fee as a constant
+    const maxTransactionFee = new Hbar(20);
+  
+    //IPFS content identifiers for which we will create a NFT
+    const CID = [
+      Buffer.from(
+        "ipfs://bafyreiao6ajgsfji6qsgbqwdtjdu5gmul7tv2v3pd6kjgcw5o65b2ogst4/metadata.json"
+      ),
+      Buffer.from(
+        "ipfs://bafyreic463uarchq4mlufp7pvfkfut7zeqsqmn3b2x3jjxwcjqx6b5pk7q/metadata.json"
+      ),
+      Buffer.from(
+        "ipfs://bafyreihhja55q6h2rijscl3gra7a3ntiroyglz45z5wlyxdzs6kjh2dinu/metadata.json"
+      ),
+      Buffer.from(
+        "ipfs://bafyreidb23oehkttjbff3gdi4vz7mjijcxjyxadwg32pngod4huozcwphu/metadata.json"
+      ),
+      Buffer.from(
+        "ipfs://bafyreie7ftl6erd5etz5gscfwfiwjmht3b52cevdrf7hjwxx5ddns7zneu/metadata.json"
+      ),
+    ];
+  
+    // MINT NEW BATCH OF NFTs
+    const mintTx = new TokenMintTransaction()
+      .setTokenId(tokenId)
+      .setMetadata(CID) //Batch minting - UP TO 10 NFTs in single tx
+      .setMaxTransactionFee(maxTransactionFee)
+      .freezeWith(client);
+  
+    //Sign the transaction with the supply key
+    const mintTxSign = await mintTx.sign(supplyKey);
+  
+    //Submit the transaction to a Hedera network
+    const mintTxSubmit = await mintTxSign.execute(client);
+  
+    //Get the transaction receipt
+    const mintRx = await mintTxSubmit.getReceipt(client);
+  
+    //Log the serial number
+    console.log(
+      `- Created NFT ${tokenId} with serial: ${mintRx.serials[0].low} \n`
+    );
+  
+    //Log the serial number
+    console.log(
+      `- Created NFT ${tokenId} with serial: ${mintRx.serials[0].low} \n`
+    );
+  
     
   }
   createFirstNft();
