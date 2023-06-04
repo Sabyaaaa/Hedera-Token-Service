456582bd31bbef9c3bbd36112f3358e4c9f8866b
sabya <sabya7011@gmail.com>
2023-06-04 12:04:34 +0530

    create nft

--- create_and_transfer_nft.js
+++ create_and_transfer_nft.js
@@ -26,3 +26,36 @@ const client = Client.forTestnet().setOperator(operatorId, operatorKey);
 
 const supplyKey = PrivateKey.generate();
 
+async function createFirstNft() {
+    //Create the NFT
+    const nftCreate = await new TokenCreateTransaction()
+      .setTokenName("metaverse")
+      .setTokenSymbol("mv")
+      .setTokenType(TokenType.NonFungibleUnique)
+      .setDecimals(0)
+      .setInitialSupply(0)
+      .setTreasuryAccountId(treasuryId)
+      .setSupplyType(TokenSupplyType.Finite)
+      .setMaxSupply(250)
+      .setSupplyKey(supplyKey)
+      .freezeWith(client);
+  
+    //Sign the transaction with the treasury key
+    const nftCreateTxSign = await nftCreate.sign(treasuryKey);
+  
+    //Submit the transaction to a Hedera network
+    const nftCreateSubmit = await nftCreateTxSign.execute(client);
+  
+    //Get the transaction receipt
+    const nftCreateRx = await nftCreateSubmit.getReceipt(client);
+  
+    //Get the token ID
+    const tokenId = nftCreateRx.tokenId;
+  
+    //Log the token ID
+    console.log(`- Created NFT with Token ID: ${tokenId} \n`);
+  
+    
+  }
+  createFirstNft();
+  
